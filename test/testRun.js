0b7ad6e27f0c128677ef3e4de073e393c57a407f
zayitloud <williez@gmail.com>
2020-02-10 23:14:52 +0100

    test uodate

--- test/testRun.js
+++ test/testRun.js
@@ -7,14 +7,19 @@ describe('fizzbuzz->Check whether the run method behaves correctly when providin
             assert.throws(() => { runner.run([]) }, (err) => { return err.message.includes('Invalid arguments passed') });
         });
     });
-    describe('#run([\'a\', \'b\', \'c\', \'d\'])', function () {
+    describe('#run([ \'c\', \'d\'])', function () {
         it('should throw an exception if no valid range is provided', function () {
-            assert.throws(() => { runner.run(['a', 'b', 'c', 'd']) }, (err) => { return err.message.includes('Invalid numbers passed') });
+            assert.throws(() => { runner.run(['c', 'd']) }, (err) => { return err.message.includes('Invalid numbers passed') });
         });
     });
-    describe('#run([\'a\', \'b\', 2, 10])', function () {
+    describe('#run([ -1, -7])', function () {
+        it('should throw an exception if no valid range is provided', function () {
+            assert.throws(() => { runner.run([-1, -7]) }, (err) => { return err.message.includes('Invalid range passed') });
+        });
+    });
+    describe('#run([ 2, 10])', function () {
         it('does not throw an exception if a valid range is provided', function () {
-            assert.doesNotThrow(() => { runner.run(['a', 'b', 2, 10]) });
+            assert.doesNotThrow(() => { runner.run([2, 10]) });
         });
     });
 })
