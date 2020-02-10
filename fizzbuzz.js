47bd75d69231cd14c328dad021fdca01c2fcbf7a
zayitloud <williez@gmail.com>
2020-02-10 23:14:46 +0100

    refactoring to accept only range instead of all the arguments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,6 @@
 const runner = require('./service/runner')
-console.log(runner.run(process.argv))
+console.log(runner.run(process.argv.slice(2)))
+
 
 
 
--- service/runner.js
+++ service/runner.js
@@ -1,12 +1,15 @@
 const calc = require('./calculator')
 
 const run = (args) => {
-    if (!args || args.length != 4) {
+    if (!args || args.length != 2) {
         throw new Error('Invalid arguments passed')
     }
-    const range = args.slice(2).map(n => parseInt(n))
+    const range = args.map(n => parseInt(n))
     if (isNaN(range[0]) || isNaN(range[1])) {
         throw new Error('Invalid numbers passed')
+    }
+    if (range[0] >= range[1]) {
+        throw new Error('Invalid range passed')
     }
     const numbers = []
     for (let i = range[0]; i <= range[1]; i++) {
